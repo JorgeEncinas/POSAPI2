0fe1edcfa1ca8c45aa7d752e34288aadec75cbba
centeno9 <fjcenteno99@gmail.com>
2021-10-10 19:42:07 -0700

    sumar cantidades y eliminar seleccion

--- POSAPI2/Form1.cs
+++ POSAPI2/Form1.cs
@@ -19,7 +19,7 @@ namespace POSAPI2
         {
             InitializeComponent();
             dgv1.Columns.Add("cantidad", "Cantidad");
-            dgv1.Columns.Add("nombre","Nombre");
+            dgv1.Columns.Add("nombre", "Nombre");
             dgv1.Columns.Add("precio", "Precio");
             dgv1.Columns.Add("total", "Total");
             textBox1.Focus();
@@ -29,27 +29,58 @@ namespace POSAPI2
         {
             labelTotal.Location = new Point(Convert.ToInt32(dgv1.Width - labelTotal.Width), Convert.ToInt32(dgv1.Height + labelTotal.Height + 20));
             labelTotal.Text = "";
-            
+
         }
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == 13)
             {
-                String query = "SELECT * FROM productos WHERE producto_codigo =" + textBox1.Text;
+                String query = "SELECT * FROM productos WHERE clave  =" + textBox1.Text;
 
                 try
                 {
-                    MySqlConnection mySqlConnection = new MySqlConnection("server=127.0.0.1; user=root; database=verificador_de_precios; SSL mode=none");
+                    MySqlConnection mySqlConnection = new MySqlConnection("server=127.0.0.1; user=root; database=verpres3; SSL mode=none");
                     mySqlConnection.Open();
                     MySqlCommand mySqlCommand = new MySqlCommand(query, mySqlConnection);
                     MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
                     if (mySqlDataReader.HasRows)
                     {
                         mySqlDataReader.Read();
-                        dgv1.Rows.Add("1", mySqlDataReader.GetString(1), String.Format("{0:0.00}", mySqlDataReader.GetDouble(3)), String.Format("{0:0.00}", mySqlDataReader.GetDouble(3)));
-                        labelTotal.Text = GetTotal().ToString();
-                        textBox1.Clear();
+                        Boolean encontrado = false;
+                        try
+                        {
+
+                            foreach (DataGridViewRow row in dgv1.Rows)
+                            {
+                                if (row.Cells["nombre"].Value.ToString().Equals(mySqlDataReader.GetString(1)))
+                                {
+                                    int x = Int32.Parse(row.Cells["cantidad"].Value.ToString());
+                                    double nuevoTotal = Double.Parse(row.Cells["precio"].Value.ToString());
+                                    nuevoTotal = (x + 1) * nuevoTotal;
+                                    row.Cells["cantidad"].Value = x + 1;
+                                    row.Cells["total"].Value = nuevoTotal;
+                                    labelTotal.Text = GetTotal().ToString();
+                                    textBox1.Clear();
+                                    encontrado = true;
+                                    row.Selected = true;
+                                    break;
+                                }
+                            }
+                        }
+                        catch (Exception exc)
+                        {
+
+                        }
+                        if (encontrado == false)
+                        {
+
+                            dgv1.Rows.Add("1", mySqlDataReader.GetString(1), String.Format("{0:0.00}", mySqlDataReader.GetDouble(3)), String.Format("{0:0.00}", mySqlDataReader.GetDouble(3)));
+                            dgv1.Rows[dgv1.Rows.Count - 1].Selected = true;
+                            labelTotal.Text = GetTotal().ToString();
+                            textBox1.Clear();
+                        }
+
                     }
                     else
                     {
@@ -77,6 +108,25 @@ namespace POSAPI2
                 catch (Exception ex)
                 {
 
+                }
+            }
+
+            if (textBox1.Text.Length == 0 && e.KeyChar == 8)
+            {
+                foreach (DataGridViewRow item in this.dgv1.SelectedRows)
+                {
+                    int nuevoIndice;
+                    if (item.Index == 0)
+                    {
+                        nuevoIndice = item.Index;
+                    }
+                    else
+                    {
+                        nuevoIndice = item.Index - 1;
+                    }
+                    dgv1.Rows[nuevoIndice].Selected = true;
+                    dgv1.Rows.RemoveAt(item.Index);
+                    break;
                 }
             }
         }
@@ -117,7 +167,7 @@ namespace POSAPI2
             pbLogo.Width = Convert.ToInt32(this.Width * 0.20);
             pbLogo.Height = Convert.ToInt32(this.Height * 0.08);
             pbLogo.Location = new Point(
-                Convert.ToInt32(this.Width - this.Width*0.05 - pbLogo.Width),
+                Convert.ToInt32(this.Width - this.Width * 0.05 - pbLogo.Width),
                 Convert.ToInt32(this.Height * 0.05));
         }
 
